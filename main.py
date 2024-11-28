f272b0580e76683a48e3fe65b14595390ce1d714
Zoe-Carruette <zoe.carruette@edu.esiee.fr>
2024-11-28 09:58:05 +0000

    06-syracuse-Zoe-Carruette

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #### Fonctions secondaires
-
-
+"""
+Les fonctions secondaires
+"""
 # imports
 from plotly.graph_objects import Scatter, Figure
 
@@ -32,8 +33,15 @@ def syracuse_l(n):
         list: la suite de Syracuse de source n
     """
 
-    # votre code ici 
+    # votre code ici
     l = [ ]
+    while n!=1:
+        l.append(n)
+        if n%2==0:
+            n=n/2
+        else:
+            n=(n*3)+1
+    l.append(1)
     return l
 
 def temps_de_vol(l):
@@ -45,11 +53,9 @@ def temps_de_vol(l):
     Returns:
         int: le temps de vol
     """
-    
     # votre code ici
 
-    n = 0
-    return n
+    return len(l)
 
 def temps_de_vol_en_altitude(l):
     """Retourne le temps de vol en altitude d'une suite de Syracuse
@@ -62,8 +68,12 @@ def temps_de_vol_en_altitude(l):
     """
 
     # votre code ici
-
-    n = 0
+    u0 = l[0]
+    n=0
+    for i in l:
+        if i<u0:
+            break
+        n+=1
     return n
 
 
@@ -76,11 +86,11 @@ def altitude_maximale(l):
     Returns:
         int: l'altitude maximale
     """
-    
     # votre code ici
-    
-    n = 0
-    return n
+    am=-1
+    for i in l:
+        am=max(am,i)
+    return int(am)
 
 
 #### Fonction principale
@@ -89,11 +99,17 @@ def altitude_maximale(l):
 def main():
 
     # vos appels à la fonction secondaire ici
-    lsyr = syracuse_l(15)
+    """lsyr = syracuse_l(15)
     syr_plot(lsyr)
     print(temps_de_vol(lsyr))
     print(temps_de_vol_en_altitude(lsyr))
-    print(altitude_maximale(lsyr))
+    print(altitude_maximale(lsyr))"""
+
+    lsyr2 = syracuse_l(127)
+    syr_plot(lsyr2)
+    print(temps_de_vol(lsyr2))
+    print(temps_de_vol_en_altitude(lsyr2))
+    print(altitude_maximale(lsyr2))
 
 
 if __name__ == "__main__":
